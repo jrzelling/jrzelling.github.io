2e361fefbbd6ab5a92d4e478e0c28a55b156e303
jrzelling <jryanzelling@gmail.com>
2018-10-12 11:51:35 -0500

    hacked
    hacked

--- js/myVol.js
+++
@@ -1,6 +0,0 @@
-var myVol = new Array();
-myVol[0] = "Spontaneous Volunteer - American Red Cross 2015"
-myVol[1] = "Programming Instructor - Hackthefuture.org 01/2013 - 12/2015"
-myVol[2] = "Programming Instructor - CoderDojo Silicon Valley 01/2014 - 12/2015"
-myVol[3] = "Operations Volunteer - HackerDojo Mountain View 01/2009 - 12/2013"
-myVol[4] = "Conference Crew - QCon 11/2012"
